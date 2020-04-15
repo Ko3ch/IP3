b764cfaf0b0724f09f769de2e125d48b731eae36
Tom Koech <tomko3ch@gmail.com>
2020-04-15 17:45:59 +0300

    added toggle effect on What we do section

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
+    $(".design").click(function(){
+        $(".designInfo").slideToggle();
+    });
+    $(".dev").click(function(){
+        $(".devInfo").slideToggle();
+    });
+    $(".product").click(function(){
+        $(".managementInfo").slideToggle();
+    });
     $("#submit").click(function(event){
         var userName = $("#name").val();
         var userEmail = $("#email").val();
         var message = $("#message").val();
        
         if(userName == "" || userEmail == "" || message == ""){
-            $("#errorMessage").html("**All fields are required**").fadeOut(4000);
+            $("#errorMessage").html("**All fields are required**").fadeOut(5000);
         }
         else
-        {
+        {   
             $("#theName").html(userName);
             $("#hidden").show().fadeOut(5000);    
         }
